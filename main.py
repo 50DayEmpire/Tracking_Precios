e93707dc094162929988042bd901c9869411edd8
50DayEmpire <79984327+50DayEmpire@users.noreply.github.com>
2024-04-17 23:20:46 -0600

    Agregado Funciones a los botones

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
 import os
+from Interfaz import historial
+import funciones as f
+from PyQt6 import QtWidgets
+
+def getTexto():
+    texto = app.ventana.textEdit.toPlainText()    
+    f.agregarUrl(texto)
+
+#app = historial.Whistorial()
+
+#para abrir la ventana de URL
+instancia = QtWidgets.QApplication([])
+
+app = historial.URL()
+
+#Conecta la función al botón
+
+app.ventana.pushButton_2.clicked.connect(getTexto)
+
+instancia.exec()
+
+
 print("""
 1. Jetstereo
 2. Gallo mas Gallo
 """)
-n=int(input())
+#n=int(input())
 print("Pegue una URL")
-url = input("")
+#url = input("")
 
 #cambia la consola a la ruta del archivo actual y ejecuta el spider
 
-if n==1:
-    os.system("cd "+ os.path.dirname(os.path.abspath(__file__)) + "/tracking" +" && scrapy crawl tracking_spider_jetstereo -o precio.jsonl -a url="+url)
-elif n==2:
-    os.system("cd "+ os.path.dirname(os.path.abspath(__file__)) + "/tracking" +" && scrapy crawl tracking_spider_GMG -o precio.jsonl -a url="+url)
+# if n==1:
+#     os.system("cd "+ os.path.dirname(os.path.abspath(__file__)) + "/tracking" +" && scrapy crawl tracking_spider_jetstereo -o precio.jsonl -a url="+url)
+# elif n==2:
+#     os.system("cd "+ os.path.dirname(os.path.abspath(__file__)) + "/tracking" +" && scrapy crawl tracking_spider_GMG -o precio.jsonl -a url="+url)
