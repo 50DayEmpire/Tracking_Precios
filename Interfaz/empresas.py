9f7c8a5c86b94df69ea7a23939ae2dd1d085ba5d
Jenifer Melghem <alemelghem01@gmail.com>
2024-04-18 00:28:51 -0600

    urls correctas

--- Interfaz/empresas.py
+++ Interfaz/empresas.py
@@ -1,4 +1,3 @@
-
 from PyQt6 import QtWidgets, uic
 import sys
 
@@ -7,15 +6,15 @@ class MainWindow(QtWidgets.QDialog):
         super().__init__()
 
         # Carga el archivo .ui
-        uic.loadUi(R'C:\Users\Richard López\Documents\GitHub\Tracking_Precios\Interfaz\gui\Tracking_main.ui', self)
+        uic.loadUi(R'gui/Tracking_main.ui', self)
 
         # Cambia la imagen de fondo del label_2A
-        self.btnGallo.setStyleSheet("image: url('C:/Users/Richard López/Documents/GitHub/Tracking_Precios/Interfaz/gui/imagenes/GMG.png');")
-        self.btnColonia.setStyleSheet("image: url('C:/Users/Richard López/Documents/GitHub/Tracking_Precios/Interfaz/gui/imagenes/Colonia.png');")
-        self.btnJets.setStyleSheet("image: url('C:/Users/Richard López/Documents/GitHub/Tracking_Precios/Interfaz/gui/imagenes/Jetstereo.png');")
-        self.btnMoto.setStyleSheet("image: url('C:/Users/Richard López/Documents/GitHub/Tracking_Precios/Interfaz/gui/imagenes/Motomundo.png');")
-        self.btnLadyLee.setStyleSheet("image: url('C:/Users/Richard López/Documents/GitHub/Tracking_Precios/Interfaz/gui/imagenes/LadyLee.jpg');")
-        self.btnJordan.setStyleSheet("image: url('C:/Users/Richard López/Documents/GitHub/Tracking_Precios/Interfaz/gui/imagenes/Jordan.png');")
+        self.btnGallo.setStyleSheet("gui/imagenes/GMG.png")
+        self.btnColonia.setStyleSheet("gui/imagenes/Colonia.png")
+        self.btnJets.setStyleSheet("gui/imagenes/Jestereo.png")
+        self.btnMoto.setStyleSheet("gui/imagenes/Motomundo.png")
+        self.btnLadyLee.setStyleSheet("gui/imagenes/LadyLee.png")
+        self.btnJordan.setStyleSheet("gui/imagenes/Jordan.png")
         
 
 if __name__ == "__main__":
