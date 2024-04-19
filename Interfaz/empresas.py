b7ece8eadaee250c9e6b87c0a0ffa7fe369d8138
Jenifer Melghem <alemelghem01@gmail.com>
2024-04-19 00:00:44 -0600

    mostrar iconos

--- Interfaz/empresas.py
+++ Interfaz/empresas.py
@@ -1,4 +1,4 @@
-from PyQt6 import QtWidgets, uic
+from PyQt6 import QtWidgets, QtGui, uic
 import sys
 
 class MainWindow(QtWidgets.QDialog):
@@ -6,16 +6,7 @@ class MainWindow(QtWidgets.QDialog):
         super().__init__()
 
         # Carga el archivo .ui
-        uic.loadUi(R'gui/Tracking_main.ui', self)
-
-        # Cambia la imagen de fondo del label_2A
-        self.btnGallo.setStyleSheet("gui/imagenes/GMG.png")
-        self.btnColonia.setStyleSheet("gui/imagenes/Colonia.png")
-        self.btnJets.setStyleSheet("gui/imagenes/Jestereo.png")
-        self.btnMoto.setStyleSheet("gui/imagenes/Motomundo.png")
-        self.btnLadyLee.setStyleSheet("gui/imagenes/LadyLee.png")
-        self.btnJordan.setStyleSheet("gui/imagenes/Jordan.png")
-        
+        uic.loadUi(R'interfaz/gui/Tracking_main.ui', self)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
