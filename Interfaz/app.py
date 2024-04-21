56f64ab6030190db787effb1eccd05d7b6b7c35c
AdenLopez2002 <79984485+AdenLopez2002@users.noreply.github.com>
2024-04-21 12:11:01 -0600

    Arreglado errores de impresión

--- Interfaz/app.py
+++ Interfaz/app.py
@@ -1,8 +1,9 @@
 #para abrir la ventana del historial
+from historial import URL
 
 from historial import Whistorial 
 app = Whistorial()
 
 #para abrir la ventana de URL
-#from URL import WURL 
-#app = WURL()
+#from historial import URL 
+app = URL()
--- Interfaz/historial.py
+++ Interfaz/historial.py
@@ -1,6 +1,6 @@
 from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication
-import funciones as f
+#import funciones as f
 # from PyQt6.QtWidgets import QMessageBox
 
 class Historial():
@@ -8,14 +8,18 @@ class Historial():
         self.login = uic.loadUi("Interfaz/gui/historial.ui")
         self.login.show()
 
-class Whistorial():
-    def __init__(self):
-        self.app = QApplication([])
-        self.login = Historial() 
-        self.app.exec()
-
 
 class URL():
     def __init__(self):
         self.ventana = uic.loadUi("Interfaz/gui/URL.ui")
         self.ventana.show()
+
+
+class Whistorial():
+    def __init__(self):
+        self.app = QApplication([])
+        self.login = Historial() 
+        
+        self.app.exec()
+
+app = Whistorial()
